ffc1b78e1749eb4f0879d195355564905f9d644c
Himanshu Dadheech <19etccs026@technonjr.org>
2022-09-01 00:21:20 +0530

    slider work start 11 min

--- client/src/component/home/SlideBarComponent.jsx
+++ client/src/component/home/SlideBarComponent.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import { styled } from "@mui/material";
+import { Box, styled,Typography } from "@mui/material";
 
 const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 1,
-  },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 1,
+    items: 5,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 1,
+    items: 2,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
@@ -26,19 +21,32 @@ const Image = styled("img")({
   width: "100%",
   height: 280,
 });
-const SlideBarComponent = ({products}) => {
+const SlideBarComponent = ({ products }) => {
   return (
-    <div>
-  <Carousel
+    <Box>
+      <Box>
+        <Typography>Deal Of The Day</Typography>
+      </Box>
+
+      <Carousel
         responsive={responsive}
-       
-        
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+        containerClass="carousel-container"
+        swipeable={false}
+        draggable={false}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={4000}
+        keyBoardControl={true}
+        slidesToSlide={1}
+        centerMode={true}
       >
         {products.map((product) => (
           <Image src={product.url} alt="banner" />
         ))}
       </Carousel>
-    </div>
+    </Box>
   );
 };
- export default SlideBarComponent;
+export default SlideBarComponent;
